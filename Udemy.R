697b849f4a272e5a0ba978d4680f24542d5253be
luwoon <66871214+luwoon@users.noreply.github.com>
2024-04-26 10:45:40 +0800

    Update Udemy.R

--- Udemy.R
+++ Udemy.R
@@ -1,6 +1,6 @@
-#SECTION 2 HOMEWORK
-#Test the Law Of Large Numbers for N random normally distributed numbers with mean = 0, stdev = 1:
-#Create an R script that will count how many of these numbers fall between -1 and 1 and divide by the total quantity of N. You know that E(X) = 68.2%.
+# SECTION 2 HOMEWORK
+# Test the Law Of Large Numbers for N random normally distributed numbers with mean = 0, stdev = 1:
+# Create an R script that will count how many of these numbers fall between -1 and 1 and divide by the total quantity of N. You know that E(X) = 68.2%.
 N <- 100
 counter <- 0
 for(i in rnorm(N)){
@@ -12,42 +12,42 @@ answer <- counter/N
 answer
 
 
-#SECTION 3 HOMEWORK
-#data
+# SECTION 3 HOMEWORK
+# data
 revenue <- c(14574.49, 7606.46, 8611.41, 9175.41, 8058.65, 8105.44, 11496.28, 9766.09, 10305.32, 14379.96, 10713.97, 15433.50)
 expenses <- c(12051.82, 5695.07, 12319.20, 12089.72, 8658.57, 840.20, 3285.73, 5821.12, 6976.93, 16618.61, 10054.37, 3803.96)
-#profit for each month
+# profit for each month
 profit <- (revenue-expenses)*1000
-#profit after tax for each month (the tax rate is 30%)
+# profit after tax for each month (the tax rate is 30%)
 profitaftertax <- (.7*revenue-expenses)*1000
-#profit margin for each month - equals to profit a after tax divided by revenue
+# profit margin for each month - equals to profit a after tax divided by revenue
 profitmargin <- round((.7*revenue-expenses)/revenue*100)
-#good months - where the profit after tax was greater than the mean for the year
+# good months - where the profit after tax was greater than the mean for the year
 for(i in profitaftertax){
   print(i>mean(profitaftertax))
 }
-#bad months - where the profit after tax was less than the mean for the year
+# bad months - where the profit after tax was less than the mean for the year
 for(i in profitaftertax){
   print(i<mean(profitaftertax))
 }
-#the best month - where the profit after tax was max for the year
+# the best month - where the profit after tax was max for the year
 for(i in profitaftertax){
   print(i==max(profitaftertax))
 }
-#the worst month - where the profit after tax was min for the year
+# the worst month - where the profit after tax was min for the year
 for(i in profitaftertax){
   print(i==min(profitaftertax))
 }
 
 
-#SECTION 4 HOMEWORK
-#Seasons
+# SECTION 4 HOMEWORK
+# Seasons
 Seasons <- c("2005","2006","2007","2008","2009","2010","2011","2012","2013","2014")
 
-#Players
+# Players
 Players <- c("KobeBryant","JoeJohnson","LeBronJames","CarmeloAnthony","DwightHoward","ChrisBosh","ChrisPaul","KevinDurant","DerrickRose","DwayneWade")
 
-#Salaries
+# Salaries
 KobeBryant_Salary <- c(15946875,17718750,19490625,21262500,23034375,24806250,25244493,27849149,30453805,23500000)
 JoeJohnson_Salary <- c(12000000,12744189,13488377,14232567,14976754,16324500,18038573,19752645,21466718,23180790)
 LeBronJames_Salary <- c(4621800,5828090,13041250,14410581,15779912,14500000,16022500,17545000,19067500,20644400)
@@ -58,13 +58,13 @@ ChrisPaul_Salary <- c(3144240,3380160,3615960,4574189,13520500,14940153,16359805
 KevinDurant_Salary <- c(0,0,4171200,4484040,4796880,6053663,15506632,16669630,17832627,18995624)
 DerrickRose_Salary <- c(0,0,0,4822800,5184480,5546160,6993708,16402500,17632688,18862875)
 DwayneWade_Salary <- c(3031920,3841443,13041250,14410581,15779912,14200000,15691000,17182000,18673000,15000000)
-#Matrix
+# Matrix
 Salary <- rbind(KobeBryant_Salary, JoeJohnson_Salary, LeBronJames_Salary, CarmeloAnthony_Salary, DwightHoward_Salary, ChrisBosh_Salary, ChrisPaul_Salary, KevinDurant_Salary, DerrickRose_Salary, DwayneWade_Salary)
 rm(KobeBryant_Salary, JoeJohnson_Salary, CarmeloAnthony_Salary, DwightHoward_Salary, ChrisBosh_Salary, LeBronJames_Salary, ChrisPaul_Salary, DerrickRose_Salary, DwayneWade_Salary, KevinDurant_Salary)
 colnames(Salary) <- Seasons
 rownames(Salary) <- Players
 
-#Games 
+# Games 
 KobeBryant_G <- c(80,77,82,82,73,82,58,78,6,35)
 JoeJohnson_G <- c(82,57,82,79,76,72,60,72,79,80)
 LeBronJames_G <- c(79,78,75,81,76,79,62,76,77,69)
@@ -75,13 +75,13 @@ ChrisPaul_G <- c(78,64,80,78,45,80,60,70,62,82)
 KevinDurant_G <- c(35,35,80,74,82,78,66,81,81,27)
 DerrickRose_G <- c(40,40,40,81,78,81,39,0,10,51)
 DwayneWade_G <- c(75,51,51,79,77,76,49,69,54,62)
-#Matrix
+# Matrix
 Games <- rbind(KobeBryant_G, JoeJohnson_G, LeBronJames_G, CarmeloAnthony_G, DwightHoward_G, ChrisBosh_G, ChrisPaul_G, KevinDurant_G, DerrickRose_G, DwayneWade_G)
 rm(KobeBryant_G, JoeJohnson_G, CarmeloAnthony_G, DwightHoward_G, ChrisBosh_G, LeBronJames_G, ChrisPaul_G, DerrickRose_G, DwayneWade_G, KevinDurant_G)
 colnames(Games) <- Seasons
 rownames(Games) <- Players
 
-#Minutes Played
+# Minutes Played
 KobeBryant_MP <- c(3277,3140,3192,2960,2835,2779,2232,3013,177,1207)
 JoeJohnson_MP <- c(3340,2359,3343,3124,2886,2554,2127,2642,2575,2791)
 LeBronJames_MP <- c(3361,3190,3027,3054,2966,3063,2326,2877,2902,2493)
@@ -92,13 +92,13 @@ ChrisPaul_MP <- c(2808,2353,3006,3002,1712,2880,2181,2335,2171,2857)
 KevinDurant_MP <- c(1255,1255,2768,2885,3239,3038,2546,3119,3122,913)
 DerrickRose_MP <- c(1168,1168,1168,3000,2871,3026,1375,0,311,1530)
 DwayneWade_MP <- c(2892,1931,1954,3048,2792,2823,1625,2391,1775,1971)
-#Matrix
+# Matrix
 MinutesPlayed <- rbind(KobeBryant_MP, JoeJohnson_MP, LeBronJames_MP, CarmeloAnthony_MP, DwightHoward_MP, ChrisBosh_MP, ChrisPaul_MP, KevinDurant_MP, DerrickRose_MP, DwayneWade_MP)
 rm(KobeBryant_MP, JoeJohnson_MP, CarmeloAnthony_MP, DwightHoward_MP, ChrisBosh_MP, LeBronJames_MP, ChrisPaul_MP, DerrickRose_MP, DwayneWade_MP, KevinDurant_MP)
 colnames(MinutesPlayed) <- Seasons
 rownames(MinutesPlayed) <- Players
 
-#Field Goals
+# Field Goals
 KobeBryant_FG <- c(978,813,775,800,716,740,574,738,31,266)
 JoeJohnson_FG <- c(632,536,647,620,635,514,423,445,462,446)
 LeBronJames_FG <- c(875,772,794,789,768,758,621,765,767,624)
@@ -109,13 +109,13 @@ ChrisPaul_FG <- c(407,381,630,631,314,430,425,412,406,568)
 KevinDurant_FG <- c(306,306,587,661,794,711,643,731,849,238)
 DerrickRose_FG <- c(208,208,208,574,672,711,302,0,58,338)
 DwayneWade_FG <- c(699,472,439,854,719,692,416,569,415,509)
-#Matrix
+# Matrix
 FieldGoals <- rbind(KobeBryant_FG, JoeJohnson_FG, LeBronJames_FG, CarmeloAnthony_FG, DwightHoward_FG, ChrisBosh_FG, ChrisPaul_FG, KevinDurant_FG, DerrickRose_FG, DwayneWade_FG)
 rm(KobeBryant_FG, JoeJohnson_FG, LeBronJames_FG, CarmeloAnthony_FG, DwightHoward_FG, ChrisBosh_FG, ChrisPaul_FG, KevinDurant_FG, DerrickRose_FG, DwayneWade_FG)
 colnames(FieldGoals) <- Seasons
 rownames(FieldGoals) <- Players
 
-#Field Goal Attempts
+# Field Goal Attempts
 KobeBryant_FGA <- c(2173,1757,1690,1712,1569,1639,1336,1595,73,713)
 JoeJohnson_FGA <- c(1395,1139,1497,1420,1386,1161,931,1052,1018,1025)
 LeBronJames_FGA <- c(1823,1621,1642,1613,1528,1485,1169,1354,1353,1279)
@@ -126,13 +126,13 @@ ChrisPaul_FGA <- c(947,871,1291,1255,637,928,890,856,870,1170)
 KevinDurant_FGA <- c(647,647,1366,1390,1668,1538,1297,1433,1688,467)
 DerrickRose_FGA <- c(436,436,436,1208,1373,1597,695,0,164,835)
 DwayneWade_FGA <- c(1413,962,937,1739,1511,1384,837,1093,761,1084)
-#Matrix
+# Matrix
 FieldGoalAttempts <- rbind(KobeBryant_FGA, JoeJohnson_FGA, LeBronJames_FGA, CarmeloAnthony_FGA, DwightHoward_FGA, ChrisBosh_FGA, ChrisPaul_FGA, KevinDurant_FGA, DerrickRose_FGA, DwayneWade_FGA)
 rm(KobeBryant_FGA, JoeJohnson_FGA, LeBronJames_FGA, CarmeloAnthony_FGA, DwightHoward_FGA, ChrisBosh_FGA, ChrisPaul_FGA, KevinDurant_FGA, DerrickRose_FGA, DwayneWade_FGA)
 colnames(FieldGoalAttempts) <- Seasons
 rownames(FieldGoalAttempts) <- Players
 
-#Points
+# Points
 KobeBryant_PTS <- c(2832,2430,2323,2201,1970,2078,1616,2133,83,782)
 JoeJohnson_PTS <- c(1653,1426,1779,1688,1619,1312,1129,1170,1245,1154)
 LeBronJames_PTS <- c(2478,2132,2250,2304,2258,2111,1683,2036,2089,1743)
@@ -143,13 +143,13 @@ ChrisPaul_PTS <- c(1258,1104,1684,1781,841,1268,1189,1186,1185,1564)
 KevinDurant_PTS <- c(903,903,1624,1871,2472,2161,1850,2280,2593,686)
 DerrickRose_PTS <- c(597,597,597,1361,1619,2026,852,0,159,904)
 DwayneWade_PTS <- c(2040,1397,1254,2386,2045,1941,1082,1463,1028,1331)
-#Matrix
+# Matrix
 Points <- rbind(KobeBryant_PTS, JoeJohnson_PTS, LeBronJames_PTS, CarmeloAnthony_PTS, DwightHoward_PTS, ChrisBosh_PTS, ChrisPaul_PTS, KevinDurant_PTS, DerrickRose_PTS, DwayneWade_PTS)
 rm(KobeBryant_PTS, JoeJohnson_PTS, LeBronJames_PTS, CarmeloAnthony_PTS, DwightHoward_PTS, ChrisBosh_PTS, ChrisPaul_PTS, KevinDurant_PTS, DerrickRose_PTS, DwayneWade_PTS)
 colnames(Points) <- Seasons
 rownames(Points) <- Players
 
-#Free Throws
+# Free Throws
 KobeBryant_FT <- c(696,667,623,483,439,483,381,525,18,196)
 JoeJohnson_FT <- c(261,235,316,299,220,195,158,132,159,141)
 LeBronJames_FT <- c(601,489,549,594,593,503,387,403,439,375)
@@ -160,13 +160,13 @@ ChrisPaul_FT <- c(394,292,332,455,161,337,260,286,295,289)
 KevinDurant_FT <- c(209,209,391,452,756,594,431,679,703,146)
 DerrickRose_FT <- c(146,146,146,197,259,476,194,0,27,152)
 DwayneWade_FT <- c(629,432,354,590,534,494,235,308,189,284)
-#Matrix
+# Matrix
 FreeThrows <- rbind(KobeBryant_FT, JoeJohnson_FT, LeBronJames_FT, CarmeloAnthony_FT, DwightHoward_FT, ChrisBosh_FT, ChrisPaul_FT, KevinDurant_FT, DerrickRose_FT, DwayneWade_FT)
 rm(KobeBryant_FT, JoeJohnson_FT, CarmeloAnthony_FT, DwightHoward_FT, ChrisBosh_FT, LeBronJames_FT, ChrisPaul_FT, DerrickRose_FT, DwayneWade_FT, KevinDurant_FT)
 colnames(FreeThrows) <- Seasons
 rownames(FreeThrows) <- Players
 
-#Free Throw Attempts
+# Free Throw Attempts
 KobeBryant_FTA <- c(819,768,742,564,541,583,451,626,21,241)
 JoeJohnson_FTA <- c(330,314,379,362,269,243,186,161,195,176)
 LeBronJames_FTA <- c(814,701,771,762,773,663,502,535,585,528)
@@ -177,33 +177,33 @@ ChrisPaul_FTA <- c(465,357,390,524,190,384,302,323,345,321)
 KevinDurant_FTA <- c(256,256,448,524,840,675,501,750,805,171)
 DerrickRose_FTA <- c(205,205,205,250,338,555,239,0,32,187)
 DwayneWade_FTA <- c(803,535,467,771,702,652,297,425,258,370)
-#Matrix
+# Matrix
 FreeThrowAttempts <- rbind(KobeBryant_FTA, JoeJohnson_FTA, LeBronJames_FTA, CarmeloAnthony_FTA, DwightHoward_FTA, ChrisBosh_FTA, ChrisPaul_FTA, KevinDurant_FTA, DerrickRose_FTA, DwayneWade_FTA)
 rm(KobeBryant_FTA, JoeJohnson_FTA, CarmeloAnthony_FTA, DwightHoward_FTA, ChrisBosh_FTA, LeBronJames_FTA, ChrisPaul_FTA, DerrickRose_FTA, DwayneWade_FTA, KevinDurant_FTA)
 colnames(FreeThrowAttempts) <- Seasons
 rownames(FreeThrowAttempts) <- Players
 
-#plotting function
+# plotting function
 myplot <- function(matrix) {
   matplot(t(matrix), type="b", pch=15:18, col=c(1:4,6), main="Basketball Players Analysis")
   legend("bottomleft", inset=0.01, legend=Players, col=c(1:4,6), pch=15:18, horiz=F)
 }
 
-#visualize new matrices
+# visualize new matrices
 myplot(FreeThrows)
 myplot(FreeThrowAttempts)
 
-#free throw attempts per game
+# free throw attempts per game
 myplot(FreeThrows/Games)
 
-#free throw accuracy
+# free throw accuracy
 myplot(FreeThrows/FreeThrowAttempts)
 
-#player style patterns excluding free throws
+# player style patterns excluding free throws
 myplot((Points-FreeThrows)/FieldGoals)
 
 
-#SECTION 5 HOMEWORK
+# SECTION 5 HOMEWORK
 getwd()
 setwd("C:\\Users\\pangl\\Downloads")
 getwd()
@@ -227,20 +227,20 @@ Life_Expectancy_At_Birth_2013 <- c(75.3286585365854,60.0282682926829,51.86617073
 add1960 <- data.frame(Code=Country_Code, Life.Exp=Life_Expectancy_At_Birth_1960)
 add2013 <- data.frame(Code=Country_Code, Life.Exp=Life_Expectancy_At_Birth_2013)
 
-#merge dataframes  
+# merge dataframes  
 merged1960 <- merge(data1960, add1960, by.x="Country.Code", by.y="Code")
 merged2013 <- merge(data2013, add2013, by.x="Country.Code", by.y="Code")
 merged1960$Year <- NULL
 merged2013$Year <- NULL
 
 library(ggplot2)
-#visualize the 1960 dataset
+# visualize the 1960 dataset
 qplot(data=merged1960, x=Fertility.Rate, y=Life.Exp,colour=Region, size=I(5), alpha=I(0.6), main="Life Expectancy vs Fertility (1960)")
-#visualize the 2013 dataset
+# visualize the 2013 dataset
 qplot(data=merged2013, x=Fertility.Rate, y=Life.Exp,colour=Region, size=I(5), alpha=I(0.6), main="Life Expectancy vs Fertility (2013)")
 
 
-#SECTION 6 HOMEWORK
+# SECTION 6 HOMEWORK
 setwd("C:\\Users\\pangl\\Downloads")
 mov <- read.csv("Section6-Homework-Data.csv")
 
